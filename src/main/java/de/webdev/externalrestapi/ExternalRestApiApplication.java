2edbe58094c27920a259a7c6867720025d645631
Fabian Döz <fabian@doez.info>
2024-07-23 15:08:29 +0200

    Initial commit

---
+++ src/main/java/de/webdev/externalrestapi/ExternalRestApiApplication.java
@@ -0,0 +1,13 @@
+package de.webdev.externalrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/webdev/externalrestapi/ExternalRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.webdev.externalrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
